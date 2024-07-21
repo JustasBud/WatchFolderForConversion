100e72168187f49d1c4b32986e407f54e9208038
JustasBud <70861477+JustasBud@users.noreply.github.com>
2024-07-21 21:57:39 +0100

    Add files via upload

---
+++ dvmkv2mp4.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+file_path=$1
+file_dir=$2
+
+while true; do
+        
+        if [ -e "$file_path" ]; then
+                previous_size=$(stat -c %s "$file_path")
+                sleep 10
+                current_size=$(stat -c %s "$file_path")
+                echo $current_size
+                echo $previous_size
+                
+                if [ "$current_size" -eq "$previous_size" ]; then
+
+                        echo $file_path
+                        echo $file_dir
+                        # docker run -it -u $(id -u):$(id -g) --rm -v "$file_dir:/convert" dvmkv2mp4 -l und,eng -r # REMOVE -it for synology - no TTY
+                        docker run -u $(id -u):$(id -g) --rm -v "$file_dir:/convert" dvmkv2mp4 -l und,eng # REMOVE -it for synology - no TTY
+                        exit 0
+                        
+                else
+                        echo "NOT same file size of $file_path, sleeping"
+                        sleep 5  # Adjust the sleep duration as needed
+                fi
+
+
+        else
+                echo "File not found or inaccessible."
+        fi
+        sleep 60
+done
---
+++ watchfolder.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+cd "/volume1/SharedMedia/"
+while true
+do
+        touch  ./lastwatch
+        sleep 60
+        #Logic to remove looking in hidden folders (e.g. @eaDir in Synology)
+        # Logic added not to cut off file paths with spaces
+        find "/volume1/SharedMedia/movies" "/volume1/SharedMedia/tv" -not -path '*/[@.]*' -cnewer ./lastwatch -type f -print0 | while IFS= read -r -d '' file_path; do
+        # echo "$file_path"
+        file_dir=$(dirname "$file_path")
+        # echo $file_dir
+
+
+        shopt -s nocasematch #checks both lower/upper case
+        if [[ "$file_path" == *'.mkv'* && ( "$file_path" == *'DV'* || "$file_path" == *'DolbyVision'* || "$file_path" == *'Dolby Vision'* || "$file_path" == *'DoVi'* ) ]]; then
+        shopt -u nocasematch
+
+            echo "executing dvmkv2mp4.sh for ${file_path}"
+            
+            ./dvmkv2mp4.sh "$file_path" "$file_dir" &
+
+
+        shopt -s nocasematch #checks both lower/upper case
+        elif [[ "$file_path" == *'.mkv'* ]]; then #if not contains dolby vision, but is mkv - extract subtitles only
+        shopt -u nocasematch
+
+            echo "executing extract_subs.sh for ${file_path}"
+
+            ./extract_subs.sh "$file_path" "$file_dir" &
+
+        else
+                :
+        
+        fi
+
+
+
+
+
+
+
+
+
+    done
+# echo $file_path
+
+
+
+
+done
+
+
